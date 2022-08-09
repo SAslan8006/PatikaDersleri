85708e269ab321a03832f7ae7c6bf17d22754a83
Süleyman <suleymanaslan.8006@gmail.com>
2022-08-09 18:28:39 +0300

    C# Projeleri

---
+++ CSharpProjeleri/DaireCizme/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaireCizme
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;
+            double thickness = 0.4;
+            ConsoleColor BorderColor = ConsoleColor.Red;
+            Console.ForegroundColor = BorderColor;
+            char symbol = '*';
+            do
+            {
+                Console.Write("Enter radius:::: ");
+                if (!double.TryParse(Console.ReadLine(), out radius) || radius <= 0)
+                {
+                    Console.WriteLine("radius have to be positive number");
+                }
+            }
+            while (radius <= 0);
+            Console.WriteLine();
+            double rIn = radius - thickness, rOut = radius + thickness;
+            for (double y = radius; y >= -radius; --y)
+            {
+                for (double x = -radius; x < rOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                    {
+                        Console.Write(symbol);
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
