b3cd6ab154e1ec73e7652d7d69940d7b0c3c035e
Süleyman <suleymanaslan.8006@gmail.com>
2022-08-10 12:53:36 +0300

    Integer İkililerin Toplamı

---
+++ CSharpProjeleri/Mutlak Kare Alma/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mutlak_Kare_Alma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen aralarında boşluk bırakarak sayılarınızın girişi yapınız[orn: 5 12 13 vb.]: ");
+            string sayilar = Console.ReadLine();
+            OzelToplam(sayilar);
+            Console.ReadKey();
+        }
+
+        public static void OzelToplam(string sayilar)
+        {            
+            string[] Dizi = sayilar.Split(' ');
+            int toplam = 0,sayi=0,toplam1=0;
+            for (int i = 0; i < Dizi.Length; i ++)
+            {
+                sayi = Convert.ToInt16(Dizi[i]);
+                if  (sayi<67)
+                {
+                    toplam1 = (67-sayi)+ toplam1;
+                }
+                else
+                {
+                    toplam = (int)Math.Pow(Math.Abs(sayi - 67),2);
+                }
+            }  
+                Console.WriteLine($" {toplam1}  {toplam} ");
+
+        }
+    }
+}
