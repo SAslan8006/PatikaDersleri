035d66deec205b8be6eb726fae348492cf3784f2
Süleyman <suleymanaslan.8006@gmail.com>
2022-08-10 13:32:12 +0300

    Karakter Değiştirme

---
+++ CSharpProjeleri/Sessiz Harf/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sessiz_Harf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Kelimeleri giriniz: ");            
+            string[] Array = Console.ReadLine().Split();
+            KelimeKontrol(Array);
+            Console.ReadKey();
+        }
+
+        private static void KelimeKontrol(string[] array)
+        {
+            foreach (var item in array)
+            {
+                bool def = false;
+                for (int i = 1; i < item.Length; i++)
+                {
+                    if (harfKontrol(item[i]) == harfKontrol(item[i - 1]))
+                        def = true;
+                }
+                Console.Write(def + " ");
+            }
+
+            bool harfKontrol(char c)
+            {
+                return !"aeıioöuü".ToCharArray().Contains(c);
+            }
+
+        }
+    }
+}
