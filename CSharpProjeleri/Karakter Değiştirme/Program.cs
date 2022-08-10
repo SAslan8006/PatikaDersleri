2dc9ddc782352c809f5418ef176b2470933c4887
Süleyman <suleymanaslan.8006@gmail.com>
2022-08-10 13:00:11 +0300

    Mutlak Kare Alma

---
+++ CSharpProjeleri/Karakter Değiştirme/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karakter_Değiştirme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen cümle girişi yapınız: ");
+            string cumle = Console.ReadLine();
+            KelimeCevir(cumle);
+            Console.ReadKey();
+        }
+
+        public static void KelimeCevir(string cumle)
+        {
+
+            string[] Array = cumle.Split(' ');
+
+            foreach (string item in Array)
+            {
+                for (int i = item.Length - 1; i >= 0; i--)
+                {
+                    Console.Write(item[i]);
+                }
+                Console.Write(" ");
+            }
+
+        }
+    }
+}
