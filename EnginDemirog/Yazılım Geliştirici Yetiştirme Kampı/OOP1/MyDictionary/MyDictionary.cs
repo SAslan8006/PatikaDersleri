cc2f8c0677918d8fd3f7b97462e239e67bba7d63
Süleyman <suleymanaslan.8006@gmail.com>
2022-08-14 13:01:23 +0300

    Hafta4Ödev

---
+++ EnginDemirog/Yazılım Geliştirici Yetiştirme Kampı/OOP1/MyDictionary/MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary<T,V>
+    {
+        KeyValuePair<T,V>[] items;
+        KeyValuePair<T, V>[] tempItems;
+        public MyDictionary()
+        {
+            items= new KeyValuePair<T, V>[0];
+        }
+        public void Add(T key,V value)
+        {
+            tempItems = items;
+            items =new KeyValuePair<T, V>[tempItems.Length+1];
+            for (int i = 0; i < tempItems.Length; i++)
+            {
+                items[i] = tempItems[i];
+            }
+            items[items.Length - 1] = new KeyValuePair<T, V>(key, value);
+        }
+        public void ShowList()
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine($"{item.Key} {item.Value}");
+            }
+        }
+
+        public int Count
+        {
+            get { return items.Length; }
+        }
+
+    }
+}
---
+++ EnginDemirog/Yazılım Geliştirici Yetiştirme Kampı/OOP1/MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<string, string> SehirlerPlaka = new MyDictionary<string, string>();
+            SehirlerPlaka.Add("80", "Osmaniye");
+            SehirlerPlaka.Add("06", "Ankara");
+            SehirlerPlaka.Add("34", "İstanbul");
+
+            Console.WriteLine($"Listedeki şehir sayısı: {SehirlerPlaka.Count}");
+            Console.WriteLine("-------------------------");
+            SehirlerPlaka.ShowList();
+            Console.ReadKey();
+        }
+    }
+}
