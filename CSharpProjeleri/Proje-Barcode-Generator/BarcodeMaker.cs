e44921adc6d9421cf611b197a7f07f1cc206a676
Süleyman <suleymanaslan.8006@gmail.com>
2022-08-17 20:00:39 +0300

    Son odev

---
+++ CSharpProjeleri/Proje-Barcode-Generator/BarcodeMaker.cs
@@ -0,0 +1,27 @@
+﻿using BarcodeLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proje_Barcode_Generator
+{
+    public class BarcodeGenerator
+    {
+        static TYPE type = TYPE.PHARMACODE;
+       
+        public static Barcode BarcodeMaker(string data)
+        {
+            Barcode barcode = new Barcode(data,type);
+            barcode.Encode(type, data);
+
+            barcode.SaveImage(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) 
+                + @"\Pratikler\MiniProjects\Proje-Barcode-Generator\barcode.png", BarcodeLib.SaveTypes.PNG);
+            Console.WriteLine("Barkod Oluşturuldu ve kaydedildi,saklanan data => {0}",data);
+            return barcode;
+        }
+        public static string BarcodeReader(Barcode barcode) 
+        {
+            return "Barkod değeri = " + barcode.RawData + "\n";
+        }
+    }
+}
---
+++ CSharpProjeleri/Proje-Barcode-Generator/Program.cs
@@ -0,0 +1,23 @@
+﻿using BarcodeLib;
+using System;
+
+namespace Proje_Barcode_Generator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Barcode barcode = BarcodeGenerator.BarcodeMaker("777");
+            Console.WriteLine(BarcodeGenerator.BarcodeReader(barcode));
+
+            barcode = BarcodeGenerator.BarcodeMaker("11");
+            Console.WriteLine(BarcodeGenerator.BarcodeReader(barcode));
+
+            barcode = BarcodeGenerator.BarcodeMaker("11");
+            Console.WriteLine(BarcodeGenerator.BarcodeReader(barcode));
+
+            barcode = BarcodeGenerator.BarcodeMaker("777");
+            Console.WriteLine(BarcodeGenerator.BarcodeReader(barcode));
+        }
+    }
+}
