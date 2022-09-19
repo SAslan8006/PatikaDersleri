5da0f9fc0ac69ca8e2830542b8e07264710c1617
Süleyman <suleymanaslan.8006@gmail.com>
2022-09-19 17:16:05 +0300

    Lesson 24

--- GencayYıldız/Net6_Angular_MiniEticaret/EticaretClient/src/app/directives/admin/delete.directive.ts
+++ GencayYıldız/Net6_Angular_MiniEticaret/EticaretClient/src/app/directives/admin/delete.directive.ts
@@ -1,8 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Directive, ElementRef, EventEmitter, HostListener, Input, Output, Renderer2 } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { DeleteDialogComponent, DeleteState } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 import { ProductService } from 'src/app/services/common/models/product.service';
 declare var $: any;
@@ -11,7 +13,12 @@ declare var $: any;
 })
 export class DeleteDirective{
 
-  constructor(private element:ElementRef, private _renderer:Renderer2, private productService:ProductService,private spinner:NgxSpinnerService,public dialog: MatDialog) { 
+  constructor(private element:ElementRef, 
+    private _renderer:Renderer2, 
+    private httpClientService:HttpClientService,
+    private spinner:NgxSpinnerService,
+    public dialog: MatDialog,
+    private alertifyService : AlertifyService) { 
     const img=_renderer.createElement("img");
     img.setAttribute("src","../../../../../assets/delete.png");
     img.setAttribute("style","cursor:pointer;");
@@ -20,14 +27,21 @@ export class DeleteDirective{
     _renderer.appendChild(element.nativeElement,img);
   }
   @Input() id:string;
+  @Input() controller:string;
   @Output() callback:EventEmitter<any>=new EventEmitter();
   @HostListener("click")
   async onclick(){
     this.openDialog(async()=>{
-      this.spinner.show(SpinnerType.BallScaleMultiple);
-      const td:HTMLTableCellElement=this.element.nativeElement;
-      await this.productService.delete(this.id); 
-      $(td.parentElement).animate({opacity:0, left:"+=50",height:"toogle"},700,()=>{this.callback.emit();});
+        this.spinner.show(SpinnerType.BallScaleMultiple);
+        const td:HTMLTableCellElement=this.element.nativeElement;
+        this.httpClientService.delete({controller:this.controller},this.id).subscribe(data=> {
+          $(td.parentElement).animate({opacity:0, left:"+=50",height:"toogle"},700,()=>{this.callback.emit();
+            this.alertifyService.message("Ürün başarıyla silimiştir.",{dismissOthers:true,messageType:MessageType.Success,position:Position.TopRight});
+          }); 
+      },(errorResponse:HttpErrorResponse)=>{
+        this.spinner.hide(SpinnerType.BallScaleMultiple);
+        this.alertifyService.message("Ürün silinirken beklenmedik hata ile karşılaşılmıştır.",{dismissOthers:true,messageType:MessageType.Error,position:Position.TopRight});
+      });
     });
  
   }
