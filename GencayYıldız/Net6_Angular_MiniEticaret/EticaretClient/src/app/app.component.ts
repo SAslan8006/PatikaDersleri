704cb77fa9c2c8f9432b75cbfbf2f710a96a5db3
Süleyman <suleymanaslan.8006@gmail.com>
2022-10-01 20:33:39 +0300

    Lesson 54(Dynamic Component Loading Altyapısının Kurulması)

--- GencayYıldız/Net6_Angular_MiniEticaret/EticaretClient/src/app/app.component.ts
+++ GencayYıldız/Net6_Angular_MiniEticaret/EticaretClient/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
+import { DynamicLoadComponentDirective } from './directives/common/dynamic-load-component.directive';
 import { AuthService } from './services/common/auth.service';
-import { HttpClientService } from './services/common/http-client.service';
+import { DynamicLoadComponentService } from './services/common/dynamic-load-component.service';
 import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
+import{ComponentType} from '../app/services/common/dynamic-load-component.service'
 declare var $: any
 
 @Component({
@@ -12,23 +13,25 @@ declare var $: any
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(public authService:AuthService, private toastrService : CustomToastrService ,private router:Router,httpClienService:HttpClientService){
-    httpClienService.get({
-      controller:"baskets"
-    }).subscribe(data=>{
-      debugger;
-    });
-   authService.identityCheck();
+  @ViewChild(DynamicLoadComponentDirective, { static: true })
+  dynamicLoadComponentDirective: DynamicLoadComponentDirective;
+
+  constructor(public authService: AuthService, private toastrService: CustomToastrService, private router: Router, private dynamicLoadComponentService: DynamicLoadComponentService) {
+    authService.identityCheck();
   }
-  signOut(){
+
+  signOut() {
     localStorage.removeItem("accessToken");
     this.authService.identityCheck();
     this.router.navigate([""]);
-    this.toastrService.message("Oturum kapatılmıştır!" , "Oturudum Durumu" , {
+    this.toastrService.message("Oturum kapatılmıştır!", "Oturum Kapatıldı", {
       messageType: ToastrMessageType.Warning,
       position: ToastrPosition.TopRight
     });
+  }
 
+  loadComponent() {
+    this.dynamicLoadComponentService.loadComponent(ComponentType.BasketsComponent, this.dynamicLoadComponentDirective.viewContainerRef);
   }
 }
 
---
+++ GencayYıldız/Net6_Angular_MiniEticaret/EticaretClient/src/app/services/common/dynamic-load-component.service.ts
@@ -0,0 +1,30 @@
+import { ComponentFactoryResolver, Injectable, ViewContainerRef } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DynamicLoadComponentService {
+
+  //ViewContainerRef          : Dinamik olarak yüklenecek componenti içerisinde barındıran container'dır. (Her dinamik yükleme sürecinde önceki view'leri clear etmemiz gerekmektedir.)
+  //ComponentFactory : Component'lerin instance'larını oluşturmak için kullanılan nesnedir.
+  //ComponentFactoryResolver : Belirli bir component için ComponentFactory'i resolve eden sınıftır. İçerisindeki resolveComponentFactory fonksiyonu aracılığıyla ilgili componente dair bir ComponentFactory nesnesi oluşturup, döner.
+
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) { }
+
+  async loadComponent(component: ComponentType, viewContainerRef: ViewContainerRef) {
+    let _component: any = null;
+
+    switch (component) {
+      case ComponentType.BasketsComponent:
+        _component = (await import("../../ui/components/baskets/baskets.component")).BasketsComponent;
+        break;
+    }
+
+    viewContainerRef.clear();
+    return viewContainerRef.createComponent(this.componentFactoryResolver.resolveComponentFactory(_component))
+  }
+}
+
+export enum ComponentType {
+  BasketsComponent
+}
