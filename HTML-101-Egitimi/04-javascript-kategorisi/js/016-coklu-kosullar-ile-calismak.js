db9388d35253c37f5b59545ccbefe7af44e5c3db
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2023-01-16 21:46:36 +0300

    Kosullarla Calismak Bolum Sonu Egzersizi

--- HTML-101-Egitimi/04-javascript-kategorisi/js/016-coklu-kosullar-ile-calismak.js
+++ HTML-101-Egitimi/04-javascript-kategorisi/js/016-coklu-kosullar-ile-calismak.js
@@ -2,12 +2,12 @@
 
 let userName = prompt("Kullanici Adiniz:")
 let age = prompt("Yasiniz:")
-let info = document.querySelector("#info")
+let info2 = document.querySelector("#info")
 
 if (userName && age >= 18) {
-    info.innerHTML = "ehliyet alabilirsiniz"
+    info2.innerHTML = "ehliyet alabilirsiniz"
 } else if (!userName) {
-    info.innerHTML = "Kullanici Adiniz Yok"
+    info2.innerHTML = "Kullanici Adiniz Yok"
 } else if ( !(age >= 18) ) {
-    info.innerHTML = "Yas Bilginiz Yok veya 18 Yasindan Kucuksunuz"
+    info2.innerHTML = "Yas Bilginiz Yok veya 18 Yasindan Kucuksunuz"
 }
--- HTML-101-Egitimi/04-javascript-kategorisi/js/017-ternary-operator-ile-short-if-kullanimi.js
+++ HTML-101-Egitimi/04-javascript-kategorisi/js/017-ternary-operator-ile-short-if-kullanimi.js
@@ -3,11 +3,18 @@
 // eger kullanici adin varsa yazdir yoksa kullanici bilginiz bulunamadi yaz
 
 let userName = prompt("Kullanici Bilginiz Yaziniz")
-let info = document.querySelector("#info")
+let info2 = document.querySelector("#info")
 
 // ternary kullanimi:
 // kosul ? dogruysa : yanlissa
 
 // userName.length > 0 ? userName : "Kullani Bilginiz Bulunamadi :("
 
-info.innerHTML = `${userName ? userName : "Kullani Bilginiz Bulunamadi :("}`
+info2.innerHTML = `${userName ? userName : "Kullanicı Bilginiz Bulunamadi :("}`
+var money;
+var canBuy = 
+    (money < 17) ? "Satın alamazsın..":
+    (money > 30) ? "Satın alabilirsin..":
+    "Para miktarını girmen gerekmektedir..";
+
+console.log(canBuy) 
