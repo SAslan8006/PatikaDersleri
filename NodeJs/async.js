ed2980c9953c75343e018affde1b4112eb2dc274
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2023-01-22 21:08:55 +0300

    async added

--- NodeJs/async.js
+++ NodeJs/async.js
@@ -40,3 +40,36 @@ function getData(data) {
   //Verilen alındı
   //Veriler düzenleniyor..
   //Veriler düzenlenemedi
+
+  const books = [
+    { name: "Kitap 1", author: "Yazar 1" },
+    { name: "Kitap 2", author: "Yazar 2" },
+    { name: "Kitap 3", author: "Yazar 3" },
+  ];
+  
+  const listBooks = () => {
+    books.map((book) => {
+      console.log(book.name);
+    });
+  };
+  
+  const addBook = (newBook) => {
+    const promise1 = new Promise((resolve, reject) => {
+      books.push(newBook);
+      resolve(books);
+      //reject('BIR HATA OLUSTU');
+    });
+  
+    return promise1;
+  };
+  
+  async function showBooks() {
+    try {
+      await addBook({ name: "Kitap 4", author: "Yazar 4" });
+      listBooks();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  showBooks();
