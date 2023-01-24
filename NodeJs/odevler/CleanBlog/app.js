285f818b5f02bc5fb56506f8b639ea8a0d037637
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2023-01-24 13:43:28 +0300

    CleanBlog odev 8

--- NodeJs/odevler/CleanBlog/app.js
+++ NodeJs/odevler/CleanBlog/app.js
@@ -1,14 +1,30 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
 
+//Template Engine
+app.set('view engine', 'ejs');
+
+//Middleware
+app.use(express.static('public'));
+
+//Routes
 app.get('/', (req, res) => {
-  const photo = {
-    id: 1,
-    title: 'Blog title',
-    description: 'Blog description',
-  };
-  res.send(photo);
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 
 const port = 3000;
