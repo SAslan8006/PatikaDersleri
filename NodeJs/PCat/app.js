b9f364539c817ae0dce1369fed572657c397381c
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2023-01-24 11:17:17 +0300

    express middlaware

--- NodeJs/PCat/app.js
+++ NodeJs/PCat/app.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+// const myLogger = (req, res, next) => {
+//   console.log('Middleware Log 1');
+//   next();
+// }
+
+// const myLogger2 = (req, res, next) => {
+//   console.log('Middleware Log 2');
+//   next();
+// }
+
+//Middleware
+app.use(express.static('public'));
+// app.use(myLogger);
+// app.use(myLogger2);
+
 app.get('/', (req, res) => {
-  const photo = {
-    id: 1,
-    name: 'Photo Name',
-    description: 'Photo description',
-  };
-  res.send(photo);
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 
 const port = 3000;
