f61a92a09d23720c7345dda60f400ffe054f2874
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2023-01-25 20:33:43 +0300

    son commit

--- NodeJs/PCat/controllers/photoControllers.js
+++ NodeJs/PCat/controllers/photoControllers.js
@@ -2,12 +2,22 @@ const Photo = require('../models/Photo');
 const fs = require('fs');
 
 exports.getAllPhotos = async (req, res) => {
-  const photos = await Photo.find({}).sort('-dateCreated');
+  const page = req.query.page || 1; // Başlangıç sayfamız veya ilk sayfamız.
+  const photosPerPage = 3; // Her sayfada bulunan fotoğraf sayısı
+  const totalPhotos = await Photo.find().countDocuments(); // Toplam fotoğraf sayısı
+
+  const photos = await Photo.find({}) // Fotoğrafları alıyoruz
+    .sort('-dateCreated') // Fotoğrafları sıralıyoruz
+    .skip((page - 1) * photosPerPage) // Her sayfanın kendi fotoğrafları
+    .limit(photosPerPage); // Her sayfada olmasını istediğimi F. sayısını sınırlıyoruz.
+
   res.render('index', {
-    photos,
+    photos:photos,
+    current:page,
+    pages:Math.ceil(totalPhotos/photosPerPage)
   });
 };
- 
+
 exports.getPhoto = async (req, res) => {
   const photo = await Photo.findById(req.params.id);
   res.render('photo', {
